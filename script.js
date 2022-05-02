cef22d00681288cbc0decdf9ea31bd735aeca79c
Albert2019y <104762210+Albert2019y@users.noreply.github.com>
2022-05-02 11:41:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var slideIndex = 0;
+carousel();
+
+var s = 'd'
+
+function show(a) {
+  var p = document.getElementsByClassName("stuff");
+  for (i = 0; i < p.length; i++) {
+    p[i].style.display = "none";
+  }
+  r = document.getElementById("right")
+  var x = document.getElementById(a)
+ 
+  x.style.display = "block"
+
+  if (a == "G") {
+    r.style.background = "#33853b"
+  }
+  if (a == "B") {
+    r.style.background = "#233b5c"
+  }
+  if (a == "L") {
+    r.style.background = "#9e3c32"
+  }
+  if (a == "d") {
+    r.style.background = "wheat"
+  }
+  x.style.background = "inherit"
+}
+
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.opacity = "0";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  
+  x[slideIndex-1].style.display = "block";
+  x[slideIndex-1].style.opacity = "1";
+
+  setTimeout(carousel, 5000);
+}
